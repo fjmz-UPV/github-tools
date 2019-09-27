6f742c80019022d03ce3157bf5f7bb4bb718717b
Paco (PC despacho) <fjmartin@dcom.upv.es>
2019-09-27 14:02:12 +0200

    añadiendo indicaciones y fichero csv como parámetro adicional

--- gt.py
+++ gt.py
@@ -10,8 +10,9 @@ class Gitub_Tools:
         self.ADMIN = user_credential
         self.ORG_NAME = orgname
 
-    def add_to_org(self):
-        with open('members.csv') as csv_file:
+    def add_to_org(self, fichero_csv):
+        #with open('members.csv') as csv_file:
+        with open(fichero_csv) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for user_info in csv_reader:
                 # send request and get response
@@ -26,15 +27,18 @@ class Gitub_Tools:
                     return False
                 else:
                     print('.') # print a dot to show progre
+                    print(user_info[0]+' añadido a '+self.ORG_NAME)
 
         return True
 
-    def add_to_team(self, teamname):
+    def add_to_team(self, teamname, fichero_csv):
         # send request to get teams info
         url = "https://" + self.ADMIN + "@api.github.com/orgs/" + \
                 self.ORG_NAME + "/teams"
+        print("URRRRRLLLLL"+url)        
         teams = requests.get(url).json()
         # get team id for api call
+        print(teams);
         team_id = None
         for team in teams:
             if team['name'] == teamname:
@@ -44,13 +48,15 @@ class Gitub_Tools:
             print('Team not found.')
             return
 
-        with open('members.csv') as csv_file:
+        #with open('members.csv') as csv_file:
+        with open(fichero_csv) as csv_file:        
             csv_reader = csv.reader(csv_file, delimiter=',')
             for user_info in csv_reader:
                 # put together the api call
                 url = "https://" + self.ADMIN + "@api.github.com" + "/teams/" +\
                     str(team_id) + "/members/" + user_info[0]
                 # send request
+                print("URL!!!!!" + url)
                 res = requests.put(url)
                 if DEBUG:
                     print(res.text)
@@ -68,7 +74,8 @@ class Gitub_Tools:
 
 if __name__ == "__main__":
     # input check
-    if len(sys.argv) < 5 or (sys.argv[1] == '-t' and len(sys.argv) != 6):
+    #if len(sys.argv) < 5 or (sys.argv[1] == '-t' and len(sys.argv) != 6):
+    if len(sys.argv) < 6 or (sys.argv[1] == '-t' and len(sys.argv) != 7):
         print("Invalid arguments." + PROMPT)
         exit(-1)
     # flag check
@@ -80,9 +87,9 @@ if __name__ == "__main__":
     gt = Gitub_Tools(user_credential=sys.argv[2]+":"+sys.argv[3],orgname=sys.argv[4])
     # get flags o = organization, t = team
     if sys.argv[1] == '-o':
-        status = gt.add_to_org()
+        status = gt.add_to_org(sys.argv[5])
     else:
-        status = gt.add_to_team(sys.argv[5])
+        status = gt.add_to_team(sys.argv[5], sys.argv[6])
 
     if status:
         print('Done.')
